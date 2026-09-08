6f24323e9120f8b3b148443c4729e58fa7820b6e
LUZENLUZ <luidyliraa@gmail.com>
2026-09-07 23:57:25 -0300

    DOM

---
+++ aula05/cond01.js
@@ -0,0 +1,6 @@
+var vel = 59
+console.log (`A velocidade do seu carro é ${vel}km/h`)
+if(vel > 60) {
+    console.log (`voce ultrapassou o limite de velocidade. MULTADO!`)
+}
+console.log (`Dirija sempre usando cinto de segurança`)
