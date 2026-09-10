317517f3b6dd4d6b04df6f5a3ba34ef4f99b137e
LUZENLUZ <luidyliraa@gmail.com>
2026-09-10 20:45:18 -0300

    terminei e comecei outro desafio

---
+++ desafios/desafio02/script.js
@@ -0,0 +1,24 @@
+function carregar () {
+    var msg = document.getElementById('msg')
+var img = document.getElementById('imagem')
+var data = new Date()
+var hora = data.getHours()
+msg.innerHTML = `AGORA SÃO ${hora} HORAS`
+
+
+if (hora >= 0 && hora < 12) {
+    /*bom dia*/
+    img.src = 'manhã2.jpg'
+}
+else if (hora < 18) {
+    /* boa tarde */
+   img.src = 'tarde2.jpg'
+   document.body.style.background = '#f74904b4'
+} else {
+    /* boa noite */
+   img.src = 'noite2.jpg'
+   document.body.style.background = 'gray'
+}
+}
+
+
