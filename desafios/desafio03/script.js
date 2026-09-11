7daaf35f8a428bc917194279d3d87a3c425f6ddb
LUZENLUZ <luidyliraa@gmail.com>
2026-09-10 22:01:46 -0300

    fim do desafio 2  de js

---
+++ desafios/desafio03/script.js
@@ -0,0 +1,52 @@
+function verificar() {
+var data = new Date()
+var ano = data.getFullYear()
+var fano = document.getElementById('txtano')
+var res = document.getElementById('res')
+if (fano.value.length == 0 || fano.value > ano) {
+    window.alert ('verifique os dados e tente novamente')
+} else {
+  var fsex = document.getElementsByName ('radsex')
+  var idade = ano - Number(fano.value)
+  res.innerHTML = `idade calculada ${idade}` 
+  var genero = ''
+  var img = document.createElement('img')
+  img.setAttribute('id', 'foto')
+  if (fsex[0].checked) {
+    genero = 'Homem'
+if (idade >= 0 && idade < 10) {
+    //criança
+    img.setAttribute('src', 'bb-masc.jpg')
+}
+else if (idade < 21) {
+    //jovem
+    img.setAttribute('src', 'jovem-masc.jpg' )
+} else if (idade < 50) {
+    //adulto 
+    img.setAttribute('src','adulto-masc.jpg' )
+}
+else {
+    //idoso
+    img.setAttribute('src', 'idoso-masc.jpg' )
+}
+  } else if (fsex[1].checked) {
+    genero = 'Mulher'
+    if (idade >= 0 && idade < 10) {
+    //criança
+}
+else if (idade < 21) {
+    //jovem
+} else if (idade < 50) {
+    //adulto 
+}
+else {
+    //idoso
+}
+}
+
+res.innerHTML = `<strong>Detectamos ${genero} com ${idade} anos.</strong>`
+res.appendChild(img)
+}
+
+
+}
