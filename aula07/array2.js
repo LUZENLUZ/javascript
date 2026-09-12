7dfddb49c45a44c8b73db8c2dc52eed7915ff7a4
LUZENLUZ <luidyliraa@gmail.com>
2026-09-12 15:39:29 -0300

    Update array2.js

--- aula07/array2.js
+++ aula07/array2.js
@@ -3,9 +3,8 @@ let valores = [6, 2, 3, 8, 10]
 for (let pos = 0; pos < valores.length; pos++) {
     console.log (`a position pos tem o valor ${pos}`)
 }
-    
+  */  
 
 for (let pos in valores) {
-    console.log(` ${pos} ${valores[pos]}`)
-} */
-console.log(valores.indexOf(0))
+    console.log(` ESSA É a POSIÇÃO ${pos} DENTRO DO ARRAY e o VALOR é ${valores[pos]}`)
+} 
